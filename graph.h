a3b6c3910b343581e4353034f392bca3e8518407
Rahul <rahul.26.saini@gmail.com>
2017-11-30 19:29:06 +0000

    D

--- graph.h
+++ graph.h
@@ -16,17 +16,30 @@ struct vertex {
     vertex(int s) : vNum(s), degree(0) {};
 };
 
+struct edge{
+  vertex* v1;
+  vertex* v2;
+  int edgeNumber;
+  double cost;
+  
+  edge(vertex* a, vertex* b, int e, double c) : v1(a), v2(b), edgeNumber(c), cost(c) {};
+
+  
+};
+
 class graph
 {
 public:
     typedef map<int, vertex *> vmap;
     vmap findVertex;
+    vector<edge*> edges;
     int numberOfVertices; 
     int numberOfEdges;
     bool vertexExists(int);
     bool addvertex(int);
     bool  addedge(int, int, double);
     graph(): numberOfVertices(0), numberOfEdges(0) {};
+    ~graph();
 };
 
 #endif
